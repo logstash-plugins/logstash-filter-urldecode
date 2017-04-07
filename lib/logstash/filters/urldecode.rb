f34923ff16f851e268b22a731c274b1629d02184
Guy Boertje <guy@elastic.co>
2017-04-07 17:54:40 +0100

    Fix for incompatible encodings: UTF-8 and ASCII-8BIT issue #6 (#9)
    * Fix for incompatible encodings: UTF-8 and ASCII-8BIT issue #6
    
    Fixes #6
    
    * dont need this line
    
    * retitle some tests and force encode to the value.encoding which might be ascii already
    
    FIxes #6

--- lib/logstash/filters/urldecode.rb
+++ lib/logstash/filters/urldecode.rb
@@ -14,12 +14,19 @@ class LogStash::Filters::Urldecode < LogStash::Filters::Base
   # Urldecode all fields
   config :all_fields, :validate => :boolean, :default => false
 
+  # Append values to the `tags` field when an exception is thrown
+  config :tag_on_failure, :validate => :array, :default => ["_urldecodefailure"]
+
   # Thel character encoding used in this filter. Examples include `UTF-8`
   # and `cp1252`
   #
   # This setting is useful if your url decoded string are in `Latin-1` (aka `cp1252`)
   # or in another character set other than `UTF-8`.
   config :charset, :validate => ::Encoding.name_list, :default => "UTF-8"
+
+  UnescapeRe = /((?:%[0-9a-fA-F]{2})+)/
+  UnescapePercent = '%'.freeze
+  UnescapePack = 'H*'.freeze
 
   public
   def register
@@ -29,23 +36,40 @@ class LogStash::Filters::Urldecode < LogStash::Filters::Base
 
   public
   def filter(event)
-    # If all_fields is true then try to decode them all
-    if @all_fields
-      event.to_hash.each { |name, value| event.set(name, urldecode(value)) }
-    # Else decode the specified field
-    else
-      event.set(@field, urldecode(event.get(@field)))
+    begin
+      # If all_fields is true then try to decode them all
+      if @all_fields
+        event.to_hash.each do |name, value|
+          event.set(name, urldecode(value))
+        end
+      # else decode the specified field
+      else
+        event.set(field, urldecode(event.get(@field)))
+      end
+    rescue => e
+      @tag_on_failure.each{|tag| event.tag(tag)}
     end
+
     filter_matched(event)
   end # def filter
 
+  private
+
+  def unescape(value)
+    # There is a bug in the Ruby stdlib URI.unescape
+    # this: URI.unescape("europ%C3%A9enneeuropéenne") throws the following error
+    # Encoding::CompatibilityError: incompatible encodings: ASCII-8BIT and UTF-8
+    # because Array#pack returns ASCII-8BIT encoded strings
+    # this a hybrid of URI.unescape and CGI.unescape
+    value.gsub(UnescapeRe){|s|[s.delete(UnescapePercent)].pack(UnescapePack).force_encoding(value.encoding) }
+  end
+
   # Attempt to handle string, array, and hash values for fields.
   # For all other datatypes, just return, URI.unescape doesn't support them.
-  private
   def urldecode(value)
     case value
     when String
-      escaped = URI.unescape(value)
+      escaped = unescape(value)
       return @converter.convert(escaped)
     when Array
       ret_values = []
